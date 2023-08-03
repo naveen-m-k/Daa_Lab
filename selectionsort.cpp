5fb3597c6c6bfbae12e495060480a28f0028f5a5
naveen-m-k <141338183+naveen-m-k@users.noreply.github.com>
2023-08-03 19:14:01 +0530

    selectionsort.cpp

---
+++ selectionsort.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#define SIZE 10
+using namespace std;
+
+class SelectionSort{
+    public:
+    int a[SIZE];
+    int min;
+    void printArray();
+    void getInput();
+    void sort();
+};
+
+void SelectionSort::getInput(){
+    cout<<"Before sorting : " <<endl;
+    for(int i=0;i<SIZE;i++)
+    {
+        a[i]=rand()%100;
+        cout<<a[i]<<"  ";
+    }
+    cout<<endl;
+}
+
+
+void SelectionSort::sort(){
+
+    for(int i=0;i<SIZE;i++)
+    {
+        min=i;
+        for(int j=i+1;j<SIZE;j++)
+        {
+            if(a[j]<a[min]){
+                min=j;
+            }
+        }
+        swap(a[min],a[i]);
+    }
+}
+void SelectionSort::printArray(){
+    cout<<"After sorting : " <<endl;
+    for(int i=0;i<SIZE;i++)
+    {
+        cout<<a[i]<<"  ";
+    }
+    cout<<endl;
+}
+
+
+int main()
+{
+    SelectionSort s;
+    s.getInput();
+    s.sort();
+    s.printArray();
+}
